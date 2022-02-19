dbc23fb23d9e10930ce5154df3526db87ed38b51
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-02-19 21:01:56 +0300

    try to sovle the code '

--- routers/products.js
+++ routers/products.js
@@ -3,16 +3,23 @@ const fetch = require("node-fetch");
 
 //get the products
 router.get('/', async (req, res) => {
-    let products = []
+    let products, categories = []
     await fetch('https://dummyjson.com/products')
         .then(res => res.json())
         .then(result => products = result)
         .catch(err => {
             console.error({ err });
         });
+    await fetch('https://dummyjson.com/products/categories')
+        .then(res => res.json())
+        .then(result => categories = result)
+        .catch(err => {
+            console.error({ err });
+        });
 
     res.render('products', {
-        products: products.products,
+        products: products?.products,
+        categories,
         title: 'Products'
     })
 })
